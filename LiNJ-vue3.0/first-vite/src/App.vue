82650160fa0afa6b081405de2e5e2e1ddfb1bd08
aplaaypung29 <729346015@qq.com>
2020-10-07 21:51:03 +0800

    10-7

--- LiNJ-vue3.0/first-vite/src/App.vue
+++ LiNJ-vue3.0/first-vite/src/App.vue
@@ -1,15 +1,21 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <demo></demo>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import demo from './components/demo1.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    demo
   }
 }
 </script>
